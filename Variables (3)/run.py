2b182d16f8a55b4368a9942746116eae86fe5609
nealwaga <waganeal@gmail.com>
2022-10-12 19:18:37 +0300

    variables

---
+++ Variables (3)/run.py
@@ -0,0 +1,20 @@
+a = "Neal"
+print (a)
+
+
+# Casting
+x = int(4)
+y = str(4)
+z = float (4)
+
+print (x)
+print (y)
+print (z)
+
+
+# Get The Type
+i = 8
+j = "Waga"
+
+print (type(i))
+print (type(j))
