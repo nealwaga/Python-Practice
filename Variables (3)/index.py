aa7d7532062b55c87abedeead7a99fd7d21cd7f3
nealwaga <waganeal@gmail.com>
2022-10-13 16:47:09 +0300

    output variables

---
+++ Variables (3)/index.py
@@ -0,0 +1,12 @@
+# Output Variables
+x = "Python"
+y = "is"
+z = "awesome"
+print (x, y, z)
+
+a = 5
+b = 20
+print (a + b)
+
+
+# Global Variables - Variables that are created outside of a function
