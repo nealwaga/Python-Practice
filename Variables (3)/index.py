b3ac336e3a8622c56ae59f9e59dc32c410380c15
nealwaga <waganeal@gmail.com>
2022-10-13 16:57:48 +0300

    global variables

--- Variables (3)/index.py
+++ Variables (3)/index.py
@@ -10,3 +10,17 @@ print (a + b)
 
 
 # Global Variables - Variables that are created outside of a function
+i = "fun"
+
+def myfunc():
+    print ("Python is " + i)
+
+myfunc()
+
+
+def myfunc():
+    global j
+    j = "GOAT"
+    print ("Cristiano Ronaldo is the " + j)
+
+myfunc()
