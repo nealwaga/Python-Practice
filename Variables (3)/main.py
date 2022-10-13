6c41ee4ac1163f887ceb17cfad304a5d7d54549c
nealwaga <waganeal@gmail.com>
2022-10-13 16:34:45 +0300

    variables

---
+++ Variables (3)/main.py
@@ -0,0 +1,14 @@
+# One Value to Multiple Variables
+x = y = z = "Pineapple"
+
+print (x)
+print (y)
+print (z)
+
+
+# Unpack a Collection
+fruits = ["Banana", "Mango", "Grape"]
+a, b, c = fruits
+print (a)
+print (b)
+print (c)
