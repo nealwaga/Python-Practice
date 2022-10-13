38beb64b3620559f710947fb078bb8746d51f0e5
nealwaga <waganeal@gmail.com>
2022-10-13 17:23:01 +0300

    type conversion

--- Numbers (5)/run.py
+++ Numbers (5)/run.py
@@ -4,3 +4,26 @@ z = 7j
 print (type(x))
 print (type(y))
 print (type(z))
+
+
+# Type Conversion
+a = 8
+b = 5.5
+c = 9j
+
+#convert from int to float:
+e = float(a)
+
+#convert from float to int:
+f = int(b)
+
+#convert from int to complex:
+g = complex(a)
+
+print (e)
+print (f)
+print (g)
+
+print (type(e))
+print (type(f))
+print (type(g))
