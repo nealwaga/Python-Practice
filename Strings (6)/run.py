975a00c15416de514933de0bf66ea131fafd948b
nealwaga <waganeal@gmail.com>
2022-10-15 17:20:26 +0300

    format strings

---
+++ Strings (6)/run.py
@@ -0,0 +1,7 @@
+# String Format
+# We can combine strings and numbers by using the format() method
+age = 22
+txt = "My name is Shaz and I am {}."
+print (txt.format(age))
+
+# The format() method takes unlimited number of arguments, and are placed into the respective placeholders:
