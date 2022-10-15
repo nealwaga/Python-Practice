af923d647490f479daaedee0ac5e507f98692eeb
nealwaga <waganeal@gmail.com>
2022-10-15 17:24:04 +0300

    format strings

--- Strings (6)/run.py
+++ Strings (6)/run.py
@@ -5,3 +5,8 @@ txt = "My name is Shaz and I am {}."
 print (txt.format(age))
 
 # The format() method takes unlimited number of arguments, and are placed into the respective placeholders:
+quantity = 3
+itemno = 567
+price = 49.50
+myorder = "I want {} pieces of item {} for {} dollars."
+print (txt.format(quantity, itemno, price))
