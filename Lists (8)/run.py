0e6d75ea5d0aeeeb43b1178df888c1284e476bde
nealwaga <waganeal@gmail.com>
2022-10-18 18:31:35 +0300

    list length

--- Lists (8)/run.py
+++ Lists (8)/run.py
@@ -2,3 +2,8 @@
 # Lists are used to store multiple items in a single variable. Lists are created using square brackets:
 mylist = ["Sharon", "Shompa", "Mwelu"]
 print (mylist)
+
+
+# List Length
+thislist = ["Benzema", "Cristiano", "Messi", "Saka"]
+print (len(thislist))
