b72d3dfcfec05925d5f3264a7640c41bd759d2fe
nealwaga <waganeal@gmail.com>
2022-10-18 18:54:07 +0300

    change list items

---
+++ Lists (8)/main.py
@@ -0,0 +1,16 @@
+# Change Item Value
+thislist = ["Apple", "Banana", "Pineapple"]
+thislist[0] = "Mango"
+print (thislist)
+
+
+# Change a Range of Item Values
+alist = ["Mercedes", "Rolls Royce", "BMW", "Audi", "Range Rover", "VolksWagen", "Puegeot"]
+alist[1:3] = "Volvo", "Nissan"
+print (alist)
+
+
+# Insert Items - To insert a new list item, without replacing any of the existing values
+mylist = ["Arsenal", "Real Madrid", "Barcelona", "Liverpool"]
+mylist.insert(2, "Manchester City")
+print (mylist)
