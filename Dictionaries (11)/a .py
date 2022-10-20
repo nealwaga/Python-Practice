1ab044e30e30c3684030f7e407a78ace2eef5a95
nealwaga <waganeal@gmail.com>
2022-10-20 18:03:29 +0300

    dictionaries

---
+++ Dictionaries (11)/a .py
@@ -0,0 +1,9 @@
+# Dictionaries are used to store data values in key:value pairs and can be referred to by using the key name. Example:
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print (thisdict)
+# or
+print (thisdict["model"])
