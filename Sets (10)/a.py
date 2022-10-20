228027422398ce2dc736c4121f2ffcd7e7c8f9f2
nealwaga <waganeal@gmail.com>
2022-10-20 17:34:47 +0300

    sets

---
+++ Sets (10)/a.py
@@ -0,0 +1,16 @@
+# Sets are used to store multiple items in a single variable.
+# Unordered means that the items in a set do not have a defined order.
+# Set items are unchangeable, meaning that we cannot change the items after the set has been created.
+# Sets cannot have two items with the same value.
+myset = {"Apple", "Banana", "Cherry"}
+print (myset)
+
+
+# Duplicates Not Allowed
+thisset = {"Mercedes", "Honda", "BMW", "Honda"}
+print (thisset)
+
+
+# The set() Constructor
+aset = set(("Arsenal", "Real Madrid", "Liverpool"))
+print (aset)
