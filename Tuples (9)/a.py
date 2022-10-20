1da92bc9b8bcaa9ebf99f3c11e002efc92a49c4b
nealwaga <waganeal@gmail.com>
2022-10-20 17:09:46 +0300

    tuples

---
+++ Tuples (9)/a.py
@@ -0,0 +1,15 @@
+# Tuples are used to store multiple items in a single variable.
+# A tuple is a collection which is ordered and unchangeable.
+# Tuples are written with round brackets. Example:
+thistuple = ("Apple", "Banana", "Cherry")
+print (thistuple)
+
+
+# Tuple Length
+atuple = ("Maryann", "Sharon", "Veronica", "Sophie")
+print (len(atuple))
+
+
+# The tuple() Constructor
+thetuple = tuple(("Mercedes", "BMW", "Audi", "Range Rover"))
+print (thetuple)
