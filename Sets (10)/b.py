9b451ff8b11b67ac213a6b44d231ba5d2b6015a0
nealwaga <waganeal@gmail.com>
2022-10-20 17:40:28 +0300

    add set items

---
+++ Sets (10)/b.py
@@ -0,0 +1,13 @@
+# Access Items.
+# You cannot access items in a set by referring to an index or a key, but you can loop through. Example:
+thisset = {"Lemon", "Citrus", "Guava"}
+for x in thisset:
+    print (x)
+# or
+print ("Citrus" in thisset)    
+
+
+# Add Items
+aset = {"Blue", "Red", "Green", "Yellow"}
+aset.add("Purple")
+print (aset)
